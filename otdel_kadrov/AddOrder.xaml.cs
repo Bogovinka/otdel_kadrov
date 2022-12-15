3a19ed4fe89ca9903d9667751dadb74db3ded0b1
Bogovinka <77607705+Bogovinka@users.noreply.github.com>
2022-12-16 00:27:04 +0500

    Initial commit

---
+++ otdel_kadrov/AddOrder.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace otdel_kadrov
+{
+    /// <summary>
+    /// Логика взаимодействия для AddOrder.xaml
+    /// </summary>
+    public partial class AddOrder : Window
+    {
+        Database1Entities db = new Database1Entities();
+        public AddOrder()
+        {
+            InitializeComponent();
+            postT.ItemsSource = db.Post.ToList();
+        }
+
+        private void addO_Click(object sender, RoutedEventArgs e)
+        {
+            if (surnameT.Text != "" && nameT.Text != "" && phoneT.Text != "" && postT.SelectedIndex > -1)
+            {
+                People p = new People()
+                {
+                    Surname = surnameT.Text,
+                    Name = nameT.Text,
+                    Phone = phoneT.Text,
+                };
+                db.People.Add(p);
+                db.SaveChanges();
+                NewWorkers nW = new NewWorkers()
+                {
+                    People_id = p.ID,
+                    Answer_id = 1,
+                    WorkPost_id = Convert.ToInt32(postT.SelectedValue)
+                };
+                db.NewWorkers.Add(nW);
+                db.SaveChanges();
+                MessageBox.Show("Заявка успешно отправлена");
+                Close();
+            }
+            else MessageBox.Show("Заполни значения БЫДЛО");
+        }
+    }
+}
---
+++ otdel_kadrov/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace otdel_kadrov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginB_Click(object sender, RoutedEventArgs e)
+        {
+            Database1Entities db = new Database1Entities();
+            if (db.Logins.Where(x => x.Login == loginText.Text && x.Password == passwordText.Password).Count() > 0)
+            {
+                AdminMenu sM = new AdminMenu();
+                sM.Show();
+                Close();
+            }
+            else MessageBox.Show("Такого логина или пароля не существует");
+        }
+
+        private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            AddOrder aO = new AddOrder();
+            aO.Show();
+        }
+    }
+}
